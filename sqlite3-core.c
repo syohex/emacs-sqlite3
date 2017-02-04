c4868022489de8dca00a08e466cd4022562c39ec
Syohei YOSHIDA <syohex@gmail.com>
2017-02-04 23:35:23 +0900

    Suppress compiler warning

--- sqlite3-core.c
+++ sqlite3-core.c
@@ -15,7 +15,7 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define _BSD_SOURCE
+#define _DEFAULT_SOURCE
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
